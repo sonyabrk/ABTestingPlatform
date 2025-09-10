d7025c965e5492120c3a04bb806edeb34dd9752c
sS <ss@MacBook-Air-sonya.local>
2025-09-10 13:05:09 -0700

    feat: изменен файл config.go для работы с логгером и добавлена новая внешняя зависимость

--- db/models/config.go
+++ db/models/config.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
 type Config struct {
 	Database struct {
 		Host     string `yaml:"host"`
@@ -11,7 +17,37 @@ type Config struct {
 	} `yaml:"database"`
 
 	Logging struct {
-		Level string `yaml:"level"`
-		File  string `yaml:"file"`
+		Level      string `yaml:"level"`       // уровень логирования (info/error)
+		File       string `yaml:"file"`        // путь к файлу логов
+		MaxSize    int64  `yaml:"max_size"`    // макс. размер файла (байты)
+		MaxBackups int    `yaml:"max_backups"` // макс. количество бэкапов
 	} `yaml:"logging"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	config := &Config{}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	d := yaml.NewDecoder(file)
+	if err := d.Decode(config); err != nil {
+		return nil, err
+	}
+
+	// установка значений по умолчанию для логирования
+	if config.Logging.MaxSize == 0 {
+		config.Logging.MaxSize = 10 * 1024 * 1024 // 10MB по умолчанию
+	}
+	if config.Logging.MaxBackups == 0 {
+		config.Logging.MaxBackups = 5 // 5 бэкапов по умолчанию
+	}
+	if config.Logging.Level == "" {
+		config.Logging.Level = "info" // info по умолчанию
+	}
+
+	return config, nil
 }
